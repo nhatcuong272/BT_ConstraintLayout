616ee3d12cfc31f9169f36ab18b38e666ca7d58f
“NhatCuongThaigit config --global user.email “nhatcuong272@icloud.com <36074969+nhatcuong272@users.noreply.github.com>
2018-02-02 20:11:49 +0700

    Initial commit

---
+++ app/src/main/java/android/example/com/bt_constraintlayout_imgcontrol/Images.java
@@ -0,0 +1,34 @@
+package android.example.com.bt_constraintlayout_imgcontrol;
+
+/**
+ * Created by HV on 2/2/2018.
+ */
+
+public class Images {
+    int id;
+    String description;
+
+    //atl+enter
+    public Images(int id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+}
